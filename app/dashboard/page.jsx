43f490ad29f8917fd6906c3bb6e6d4f2181a2644
Igor Bastos <114706936+IgorBastos19@users.noreply.github.com>
2024-11-04 10:28:39 -0300

    acaba com not found dashboard

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,15 @@
+export default function dashboard() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#F5F5F5",
+      }}
+    >
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
